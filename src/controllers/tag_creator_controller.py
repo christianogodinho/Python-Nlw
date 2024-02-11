e9f9390ce4769b5a44a99bee522097194ce30200
christianogodinho <christiano_godinho@id.uff.br>
2024-02-11 19:53:11 -0300

    test: Implementing unittests with pytest

--- src/controllers/tag_creator_controller.py
+++ src/controllers/tag_creator_controller.py
@@ -14,7 +14,7 @@ class TagCreatorController:
 
     def __create_tag(self, product_code:str) -> str:
         barcode_handler = BarcodeHandler()
-        path_from_tag = barcode_handler.creat_barcode(product_code)
+        path_from_tag = barcode_handler.create_barcode(product_code)
 
         return path_from_tag
 
--- src/drivers/barcode_handler.py
+++ src/drivers/barcode_handler.py
@@ -2,7 +2,7 @@ from barcode import Code128
 from barcode.writer import ImageWriter
 
 class BarcodeHandler:
-    def creat_barcode(self, product_code: str) -> str:
+    def create_barcode(self, product_code: str) -> str:
         tag = Code128(product_code, writer=ImageWriter())
         path_from_tag=f'{tag}'
         tag.save(path_from_tag)
